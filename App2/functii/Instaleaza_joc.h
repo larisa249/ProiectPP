2632ceb7e586f5587f108e398209d9bcb0c7eef0
larisa249 <119445704+larisa249@users.noreply.github.com>
2023-06-08 17:55:57 +0300

    Add files via upload

---
+++ App2/functii/Instaleaza_joc.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#ifndef INSTALEAZA_JOC_H
+#define INSTALEAZA_JOC_H 
+
+
+void instaleaza_joc(const string id){
+  cout<<"Jocul " << id << " a fost instalat\n";
+}
+#endif
